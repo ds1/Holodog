43219f2b1a34ea9906c37f0509e08874bd07376a
Dan Schmitz <danmakesthings@gmail.com>
2017-05-22 12:50:37 -0500

    Recreated repo

---
+++ Assets/Scripts/ListOfTextures.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListOfTextures : MonoBehaviour {
+
+	public List<MovieTexture> movTextures = new List<MovieTexture>();
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("A")) {
+			Debug.Log ("a was pressed");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayMovieOnSpace.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+	public class PlayMovieOnSpace : MonoBehaviour {
+		
+	public List<MovieTexture> movTextures = new List<MovieTexture>();
+
+	void Update () {
+			if (Input.GetButtonDown ("Jump")) {
+
+				Renderer r = GetComponent<Renderer>();
+				MovieTexture movie = r.material.mainTexture as MovieTexture;
+
+				if (movie.isPlaying) {
+					movie.Pause();
+				}
+				else {
+					movie.Play();
+				}
+			}
+		}
+}
